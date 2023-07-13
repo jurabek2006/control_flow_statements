092ca1f1f00142e7adeee39b3017c88c2b998ef5
jurabek2006 <yuldoshevjurabek@gmail.com>
2023-07-13 10:47:01 +0500

    first

--- bin/if01.dart
+++ bin/if01.dart
@@ -7,4 +7,15 @@
     Returns:
         a: a increased by 1 if positive, else unchanged.
  */
-void main() {}
+
+func(int number){
+    if(number>0){
+        return number+1;
+    }
+    if(number<0){
+        return number;
+    }
+}
+void main() {
+    print(func(-3));
+}
